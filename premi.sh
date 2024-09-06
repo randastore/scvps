2fd2822d840e928a9ef0a5137d185574a3a5b758
randastore <159535714+randastore@users.noreply.github.com>
2024-09-06 13:47:13 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -128,7 +128,7 @@ fi
 echo -e "\e[32mloading...\e[0m"
 clear
 # REPO    
-    REPO="https://raw.githubusercontent.com/RanTempest/scvps/main/"
+    REPO="https://raw.githubusercontent.com/randastore/scvps/main/"
 
 ####
 start=$(date +%s)
